02c1cc59e2c399af215499448ef84943f886461f
Nastya <himeriya97@mgmail.com>
2023-08-03 18:01:44 +0300

    feat: add drag and drop

--- todo/src/app/modules/core/components/todo-list/todo-list.component.ts
+++ todo/src/app/modules/core/components/todo-list/todo-list.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { TodoService } from '../../services/todo.service';
 import { Subscription } from 'rxjs';
 import { TodoItemInterface } from '../../models/todo-item.model';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-todo-list',
@@ -9,15 +14,23 @@ import { TodoItemInterface } from '../../models/todo-item.model';
   styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit, OnDestroy {
+  @Input() isActiveList = false;
+
   private subscription!: Subscription;
   public todolist: TodoItemInterface[] = [];
+  public listId = '';
+  public secondListId: string[] = [];
 
   constructor(private todoservice: TodoService) {}
 
   public ngOnInit(): void {
     this.subscription = this.todoservice.todolist$.subscribe(list => {
-      this.todolist = list;
+      this.todolist = list.filter(val =>
+        this.isActiveList ? val.status === 'in progress' : val.status === 'done'
+      );
     });
+    this.listId = this.isActiveList ? 'todoList' : 'doneList';
+    this.secondListId = this.isActiveList ? ['doneList'] : ['todoList'];
   }
 
   public ngOnDestroy(): void {
@@ -30,5 +43,25 @@ export class TodoListComponent implements OnInit, OnDestroy {
 
   public removeItem(item: TodoItemInterface): void {
     this.todoservice.removeItem(item);
+  }
+
+  public drop(event: CdkDragDrop<TodoItemInterface[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+      this.todoservice.changeItemStatus(
+        event.container.data[event.currentIndex]
+      );
+    }
   }
 }
--- todo/src/app/modules/material/material.module.ts
+++ todo/src/app/modules/material/material.module.ts
@@ -6,6 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [],
@@ -17,6 +18,7 @@ import { MatIconModule } from '@angular/material/icon';
     MatCardModule,
     MatCheckboxModule,
     MatIconModule,
+    DragDropModule,
   ],
 })
 export class MaterialModule {}
